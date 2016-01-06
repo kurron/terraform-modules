571a588a3a41874d86227f900740e1b99a7fe584
Ron Kurr <kurr@kurron.org>
2016-01-06 02:29:04 +0000

    Showed how inputs should be provided

--- main.tf
+++ main.tf
@@ -7,4 +7,7 @@ module "aws-vpc" {
     source = "aws-vpc"
     aws_region = "${var.aws_region}"
     name = "Example VPC"
+    realm = "${var.realm}"
+    purpose = "${var.purpose}"
+    managed_by = "${var.managed_by}"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "aws_region" {
     description = "AWS region to launch servers."
     default = "us-west-2"
 }
+
+variable "realm" {
+    description = "The logical group that all of the infrastructure belongs to. Similar idea to an AWS stack."
+    default = "module-experimentation" 
+}
+
+variable "purpose" {
+    description = "A tag indicating why all the infrastructure exists, eg. load-testing."
+    default = "Terraforming" 
+}
+
+variable "managed_by" {
+    description = "The tool that manages this resource."
+    default = "Terraform" 
+}
