42d1ae87b1e9570229459cf28bd8ada6a777193d
Ron Kurr <kurr@kurron.org>
2016-01-07 00:47:22 +0000

    Enabled DNS support

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
     cidr_block = "${var.cidr_block}"
+    enable_dns_support = true
     enable_dns_hostnames = "${var.enable_dns_hostnames}"
     instance_tenancy = "${var.instance_tenancy}"
 
