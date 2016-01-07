acaa00f6d336e3a0d2780b1c7906778e1fc06a7f
Ron Kurr <kurr@kurron.org>
2016-01-07 01:32:39 +0000

    Added an extra subnet

--- aws-vpc/inputs.tf
+++ aws-vpc/inputs.tf
@@ -20,17 +20,17 @@ variable "instance_tenancy" {
 
 variable "public_subnets" { 
     description = "A list of subnets that can be accessed via the internet."
-    default = "10.0.10.0/24,10.0.30.0/24" 
+    default = "10.0.10.0/24,10.0.30.0/24,10.0.50.0/24" 
 }
 
 variable "private_subnets" { 
     description = "A list of subnets that cannot be accessed via the internet."
-    default = "10.0.20.0/24,10.0.40.0/24" 
+    default = "10.0.20.0/24,10.0.40.0/24,10.0.60.0/24"
 }
 
 variable "availability_zones" { 
     description = "A list of availability zones we want to be in."
-    default = "us-west-2a,us-west-2b,us-west-2c" 
+    default = "us-west-2a,us-west-2b,us-west-2c"
 }
 
 variable "subnet_name" {
