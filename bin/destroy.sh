8c6b79bfcdadc6acaaef846a823ec5dda52375d3
Ron Kurr <kurr@kurron.org>
2017-09-02 18:54:52 -0400

    Can plan and destroy now

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # because the container isn't bound to stdin, we can't tell it do destroy or not
-terraform destroy -refresh=true -force $*
+#terraform destroy -refresh=true -force $*
+
+DESTROY="terraform destroy -refresh=true -input=false -var-file=development/networking/vpc.tfvars modules/networking/vpc/"
+echo ${DESTROY}
+${DESTROY}
