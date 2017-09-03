13d2f7217ff7bf66ca60abd40327bef7d854744b
Ron Kurr <kurr@kurron.org>
2017-09-03 17:57:23 -0400

    Got remote backend working

--- development/networking/initialize.sh
+++ development/networking/initialize.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-INIT="terraform init -backend-config='bucket=transparent-terraform-state' \
-                     -backend-config='key=development/networking/vpc' \
-                     -backend-config='region=us-east-1' \
+INIT="terraform init -backend-config=backend.cfg \
                      ../../modules/networking/vpc/"
 echo ${INIT}
 ${INIT}
