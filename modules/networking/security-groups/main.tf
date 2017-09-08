3099607d0d8d3e9febd4b33ac9b80690fbb21a92
Ron Kurr <kurr@kurron.org>
2017-09-08 11:13:41 -0400

    Added API Gateway security group

--- modules/networking/security-groups/main.tf
+++ modules/networking/security-groups/main.tf
@@ -19,13 +19,28 @@ data "terraform_remote_state" "vpc" {
 }
 
 resource "aws_security_group" "bastion_access" {
-    name_prefix = "${var.project}-bastion-"
+    name_prefix = "bastion-"
     description = "Controls access to the Bastion boxes"
     vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
     tags {
         Name        = "Bastion Access"
         Project     = "${var.project}"
         Purpose     = "Controls access to the Bastion boxes"
+        Creator     = "${var.creator}"
+        Environment = "${var.environment}"
+        Freetext    = "${var.freetext}"
+    }
+
+}
+
+resource "aws_security_group" "api_gateway_access" {
+    name_prefix = "api-gateway-"
+    description = "Controls access to the API Gateway"
+    vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+    tags {
+        Name        = "API Gateway Access"
+        Project     = "${var.project}"
+        Purpose     = "Controls access to the API Gateway"
         Creator     = "${var.creator}"
         Environment = "${var.environment}"
         Freetext    = "${var.freetext}"
--- modules/networking/security-groups/output.tf
+++ modules/networking/security-groups/output.tf
@@ -7,3 +7,13 @@ output "bastion_name" {
     value = "${aws_security_group.bastion_access.name}"
     description = "Name of the Bastion security group"
 }
+
+output "api_gateway_id" {
+    value = "${aws_security_group.api_gateway_access.id}"
+    description = "ID of the API Gateway security group"
+}
+
+output "api_gateway_name" {
+    value = "${aws_security_group.api_gateway_access.name}"
+    description = "Name of the API Gateway security group"
+}
