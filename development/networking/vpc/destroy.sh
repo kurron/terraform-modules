846361d24edeead68ad6ea8e5e6c3fbc207d3160
Ron Kurr <kurr@kurron.org>
2017-09-09 10:45:01 -0400

    Normalize the settings file name

--- development/networking/vpc/destroy.sh
+++ development/networking/vpc/destroy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DESTROY="terraform destroy -refresh=true -input=false -var-file=vpc.tfvars ../../../modules/networking/vpc/"
+DESTROY="terraform destroy -refresh=true -input=false -var-file=settings.tfvars ../../../modules/networking/vpc/"
 echo ${DESTROY}
 ${DESTROY}
--- development/networking/vpc/plan.sh
+++ development/networking/vpc/plan.sh
@@ -4,6 +4,6 @@ GET="terraform get -update=true ../../../modules/networking/vpc/"
 echo ${GET}
 ${GET}
 
-PLAN="terraform plan -refresh=true -input=false -var-file=vpc.tfvars -out=proposed-changes.plan ../../../modules/networking/vpc/"
+PLAN="terraform plan -refresh=true -input=false -var-file=settings.tfvars -out=proposed-changes.plan ../../../modules/networking/vpc/"
 echo ${PLAN}
 ${PLAN}
