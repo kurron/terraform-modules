0578be23eddd7acefdcc4ccc29772a1b6ac14941
Ron Kurr <kurr@kurron.org>
2017-09-15 16:31:40 -0400

    Removed dead variables

--- modules/compute/docker-boxes/vars.tf
+++ modules/compute/docker-boxes/vars.tf
@@ -13,11 +13,6 @@ variable "aws_secret_key" {
     description = "Secret value of the API key to use"
 }
 
-variable "cluster_name" {
-    type = "string"
-    description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores) "
-}
-
 variable "vpc_bucket" {
     type = "string"
     description = "S3 bucket containing the VPC Terraform state"
@@ -101,56 +96,6 @@ variable "freetext" {
 variable "instance_type" {
     type = "string"
     description = "Instance type to make the worker hosts from"
-}
-
-variable "spot_max_size" {
-    type = "string"
-    description = "Maximum number of worker instances that can be run simultaneously"
-}
-
-variable "spot_min_size" {
-    type = "string"
-    description = "Minimum number of work instances that can be run simultaneously"
-}
-
-variable "cooldown" {
-    type = "string"
-    description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start."
-}
-
-variable "health_check_grace_period" {
-    type = "string"
-    description = "Time, in seconds, after instance comes into service before checking health."
-}
-
-variable "spot_desired_capacity" {
-    type = "string"
-    description = "The number of workers that should be running in the group."
-}
-
-variable "spot_scale_down_desired_capacity" {
-    type = "string"
-    description = "The number of workers that should be running when scaling down."
-}
-
-variable "spot_scale_down_min_size" {
-    type = "string"
-    description = "Minimum number of workers that can be running when scaling down"
-}
-
-variable "spot_scale_up_cron" {
-    type = "string"
-    description = "In UTC, when to scale up the worker instances"
-}
-
-variable "spot_scale_down_cron" {
-    type = "string"
-    description = "In UTC, when to scale down the worker instances"
-}
-
-variable "spot_price" {
-    type = "string"
-    description = "The cost per hour you are willing to pay for a spot instance."
 }
 
 variable "ebs_optimized" {
