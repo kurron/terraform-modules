8626574242cad3bff9713f633e9a1cdb9e9b6697
Ron Kurr <kurr@kurron.org>
2017-09-19 09:43:03 -0400

    Moved policy files into their own folder.  They were starting to clutter up the folder.

--- modules/security/iam/main.tf
+++ modules/security/iam/main.tf
@@ -22,13 +22,13 @@ data "terraform_remote_state" "vpc" {
 resource "aws_iam_role" "cross_account_ecr_pull_role" {
     name_prefix        = "ecr-pull-"
     description        = "Allows EC2 instances to assume required roles"
-    assume_role_policy = "${file( "${path.module}/ecr-pull-only-role-policy.json" )}"
+    assume_role_policy = "${file( "${path.module}/files/ecr-pull-only-role-policy.json" )}"
 }
 
 resource "aws_iam_role_policy" "cross_account_ecr_pull_role_policy" {
     name_prefix = "ecr-pull-"
     role        = "${aws_iam_role.cross_account_ecr_pull_role.id}"
-    policy      = "${file("${path.module}/ecr-pull-only-policy.json")}"
+    policy      = "${file("${path.module}/files/ecr-pull-only-policy.json")}"
 }
 
 resource "aws_iam_instance_profile" "cross_account_ecr_pull_profile" {
@@ -36,23 +36,23 @@ resource "aws_iam_instance_profile" "cross_account_ecr_pull_profile" {
     role  = "${aws_iam_role.cross_account_ecr_pull_role.name}"
 }
 
-# construct a role that alloww ECS instances to interact with load balancers
+# construct a role that allow ECS instances to interact with load balancers
 resource "aws_iam_role" "default_ecs_role" {
     name_prefix = "ecs-role"
     description = "Allows ECS workers to assume required roles"
-    assume_role_policy = "${file( "${path.module}/default-ecs-role-policy.json" )}"
+    assume_role_policy = "${file( "${path.module}/files/default-ecs-role-policy.json" )}"
 }
 
 resource "aws_iam_role_policy" "default_ecs_service_role_policy" {
     name_prefix = "ecs-service-role-${var.project}-${var.environment}-"
     role = "${aws_iam_role.default_ecs_role.id}"
-    policy = "${file( "${path.module}/default-ecs-service-role-policy.json" )}"
+    policy = "${file( "${path.module}/files/default-ecs-service-role-policy.json" )}"
 }
 
 resource "aws_iam_role_policy" "default_ecs_instance_role_policy" {
     name_prefix = "ecs-instance-role-policy-${var.project}-${var.environment}-"
     role = "${aws_iam_role.default_ecs_role.id}"
-    policy = "${file( "${path.module}/default-ecs-instance-role-policy.json" )}"
+    policy = "${file( "${path.module}/files/default-ecs-instance-role-policy.json" )}"
 }
 
 resource "aws_iam_instance_profile" "default_ecs" {
