959cab8635393af94734daf6166de260ae8bdc60
Ron Kurr <kurr@kurron.org>
2017-09-22 12:34:47 -0400

    Stop Lambda deploys

--- modules/compute/scheduled-ec2-instances/main.tf
+++ modules/compute/scheduled-ec2-instances/main.tf
@@ -28,7 +28,7 @@ data "template_file" "start_lambda" {
 
 data "archive_file" "start_lambda" {
     type        = "zip"
-    output_path = "${path.module}/files/start-lambda.zip"
+    output_path = "${path.module}/archives/start-lambda.zip"
 
     source {
         content  = "${data.template_file.start_lambda.rendered}"
@@ -37,7 +37,7 @@ data "archive_file" "start_lambda" {
 }
 
 resource "aws_lambda_function" "start_lambda" {
-    filename = "${path.module}/files/start-lambda.zip"
+    filename = "${path.module}/archives/start-lambda.zip"
     function_name = "ec2-scheduled-start"
     handler = "start-lambda.py"
     role = "${data.terraform_remote_state.iam.start_stop_role_arn}"
@@ -48,6 +48,42 @@ resource "aws_lambda_function" "start_lambda" {
         Name = "EC2 Scheduled Start"
         Project = "${var.project}"
         Purpose = "Starts EC2 instances that match the specified tags"
+        Creator = "${var.creator}"
+        Environment = "${var.environment}"
+        Freetext = "${var.freetext}"
+    }
+}
+
+data "template_file" "stop_lambda" {
+    template = "${file("${path.module}/stop-lambda.py.template")}"
+    vars {
+        project = "${var.project}"
+        environment = "${var.environment}"
+    }
+}
+
+data "archive_file" "stop_lambda" {
+    type        = "zip"
+    output_path = "${path.module}/archives/stop-lambda.zip"
+
+    source {
+        content  = "${data.template_file.stop_lambda.rendered}"
+        filename = "stop-lambda.py"
+    }
+}
+
+resource "aws_lambda_function" "stop_lambda" {
+    filename = "${path.module}/archives/stop-lambda.zip"
+    function_name = "ec2-scheduled-stop"
+    handler = "stop-lambda.py"
+    role = "${data.terraform_remote_state.iam.start_stop_role_arn}"
+    description = "Turns off EC2 instances that match the specified tag values"
+    runtime = "python3.6"
+    source_code_hash = "${data.archive_file.stop_lambda.output_base64sha256}"
+    tags {
+        Name = "EC2 Scheduled Stop"
+        Project = "${var.project}"
+        Purpose = "Stops EC2 instances that match the specified tags"
         Creator = "${var.creator}"
         Environment = "${var.environment}"
         Freetext = "${var.freetext}"
--- modules/compute/scheduled-ec2-instances/output.tf
+++ modules/compute/scheduled-ec2-instances/output.tf
@@ -7,3 +7,13 @@ output "start_last_modified" {
     value = "${aws_lambda_function.start_lambda.last_modified}"
     description = "The date the start Lambda was last modified."
 }
+
+output "stop_arn" {
+    value = "${aws_lambda_function.stop_lambda.arn}"
+    description = "Amazon Resource Name (ARN) identifying the function that stops the instances."
+}
+
+output "stop_last_modified" {
+    value = "${aws_lambda_function.stop_lambda.last_modified}"
+    description = "The date the stop Lambda was last modified."
+}
